42b1733885f62712a92eb4389fd439ac75ceaaa3
Anton Grundäng <anton.grundang@gmail.com>
2025-06-09 16:57:08 +0200

    BLEAH

--- src/dht11.c
+++ src/dht11.c
@@ -198,7 +198,7 @@ uint8_t DHT11_getHumid(void){
  * @brief Returns DTH11 temp integral. Call DTH11_readData() first
  * @return temp val decimal
  */
-uint8_t getTempIntegral(void){
+uint8_t DHT11_getTempIntegral(void){
 	return data.temp_integral;
 }
 
@@ -206,6 +206,6 @@ uint8_t getTempIntegral(void){
  * @brief Returns DTH11 temp decimal. Call DTH11_readData() first
  * @return temp val integral
  */
-uint8_t getTempDecimal(void){
+uint8_t DHT11_getTempDecimal(void){
 	return data.temp_decimal;
 }
